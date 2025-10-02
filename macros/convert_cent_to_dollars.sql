c29fcffba7fab7532b4292d876e2a72bb3fad468
victor.plet@adone.fr <victor.plet@adone.fr>
2025-10-02 14:53:30 +0000

    pré merge PRD

---
+++ macros/convert_cent_to_dollars.sql
@@ -0,0 +1,7 @@
+{% macro convert_cents(ordered_quantity,euro=True,rate=0.85) %}
+    {% if euro %}
+        {{rate}}*{{ordered_quantity}} / 100 as ordered_quantity_converted
+    {% else %}
+        {{ordered_quantity}} / 100 as ordered_quantity_converted
+    {% endif %}
+{% endmacro %}
--- models/stripe/Fact/fact_payments.sql
+++ models/stripe/Fact/fact_payments.sql
@@ -4,5 +4,6 @@
     payment_method,
     status,
     order_date,
-    ordered_quantity
+    ordered_quantity,
+    {{convert_cents('ordered_quantity',euro = 'euro',rate = 0.85)}}
 from {{ ref("stg_payments") }}
